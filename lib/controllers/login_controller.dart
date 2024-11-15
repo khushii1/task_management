5e950a70a60c6f4a385b91133ae9b61d1c9194b0
Khushi Agarwal <khushiagarwal270203@gmail.com>
2024-11-15 11:10:56 +0530

    login auth

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
@@ -8,22 +9,39 @@ import '../utilities/custom_field.dart';
 class LoginController extends GetxController{
   final email=TextEditingController();
   final password=TextEditingController();
+  final databases = Databases(Datainfo.client!);
   Future<void> login({required BuildContext context}) async {
-    print("yes done");
+    print("Checking user in database...");
     try {
-       await Datainfo.account.createEmailPasswordSession(
 
+      final response = await databases.listDocuments(
+        databaseId: '6735eba3001840aef863',
+        collectionId: '6735ebaa003ba5e26526',
+        queries: [
+          Query.equal('email', email.text),
+          Query.equal('password', password.text),
+        ],
+      );
+
+      if (response.documents.isEmpty) {
+        // No matching user found in the database
+        showSnackBar(message: "Invalid email or password", context: context);
+        return;
+      }
+
+
+      await Datainfo.account.createEmailPasswordSession(
         email: email.text,
-        password:password.text,
-
-      ).then((value){
-
-        showSnackBar(message: "loged in", context: context);
+        password: password.text,
+      ).then((value) {
+        showSnackBar(message: "Logged in successfully", context: context);
         context.go("/jioscreen");
       });
 
     } catch (e) {
+
       showSnackBar(message: e.toString(), context: context);
     }
   }
+
 }
