3f947e57c300499d14743f0c762beee2b0f0782c
nishantsharma113 <nishantshara1198@gmail.com>
2024-11-18 15:12:12 +0530

    changes1

--- lib/screens/main_screens/bars/my_space/myspace_screen.dart
+++ lib/screens/main_screens/bars/my_space/myspace_screen.dart
@@ -1,3 +1,5 @@
+import 'package:jio_works/utilities/utilities.dart';
+
 import '../../../../utilities/library.dart';
 
 class MyspaceScreen extends StatelessWidget {
@@ -5,11 +7,25 @@ class MyspaceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Expanded(
-       child: Container(
-        color: Colors.blue,
-
-           ),
-     );
+    return Expanded(
+        child: Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextWidget(
+            text: getGreeting(),
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+          ),
+          TextWidget(
+            text: getFormattedDate(),
+            fontSize: 20,
+            color: textColor,
+          )
+        ],
+      ).p16(),
+    ).pSymmetric(h: 5.0, v: 10.0));
   }
 }
