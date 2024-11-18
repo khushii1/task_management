8305a23c3a3da5be2180788569e93adae3bd697b
nishantsharma113 <nishantshara1198@gmail.com>
2024-11-18 15:14:13 +0530

    changes2

--- lib/constants/color-constant.dart
+++ lib/constants/color-constant.dart
@@ -1,11 +1,12 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xff0078ad);
-Color Color1=Color(0xffe9f7e9);
-Color Color2=Color(0xffffeaed);
-Color Color3=Color(0xfffef7e9);
-Color Color4=Color(0xfffef0e7);
-Color Color5=Color(0xffebebeb);
+Color color1=const Color(0xffe9f7e9);
+Color color2=const Color(0xffffeaed);
+Color color3=const Color(0xfffef7e9);
+Color color4=const Color(0xfffef0e7);
+Color color5=const Color(0xffebebeb);
 
 
 Color textColor = const Color(0xff595959);
