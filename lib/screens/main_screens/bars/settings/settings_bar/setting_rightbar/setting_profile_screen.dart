15a623e046cc5a70d956f0380d1aaa0b7d5c8c2a
Khushi Agarwal <khushiagarwal270203@gmail.com>
2024-11-18 14:44:14 +0530

    setting_profile

--- lib/screens/main_screens/bars/settings/settings_bar/setting_rightbar/setting_profile_screen.dart
+++ lib/screens/main_screens/bars/settings/settings_bar/setting_rightbar/setting_profile_screen.dart
@@ -7,6 +7,27 @@ class SettingProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: Container(color: Colors.blue,));
+    return Expanded(child: Container(
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextWidget(text: "Profile Details",
+            fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+            10.heightBox,
+            TextWidget(text: "Basic info like your name, photo, email that you use on jioworks service",fontSize: 18,)
+          , 10.heightBox,
+            Card(
+              child: Column(
+                children: [],
+              ),
+            ).pSymmetric(h: 10,v: 10)
+          ],
+        ),
+      ),
+      
+    )).pSymmetric(v: 15);
   }
 }
