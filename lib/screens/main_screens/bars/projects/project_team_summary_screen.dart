e6c20d82ad5c3c20a009356ee559c11b5d98ec3a
Khushi Agarwal <khushiagarwal270203@gmail.com>
2024-11-28 11:26:58 +0530

    changes

--- lib/screens/main_screens/bars/projects/project_team_summary_screen.dart
+++ lib/screens/main_screens/bars/projects/project_team_summary_screen.dart
@@ -12,6 +12,7 @@ class ProjectTeamSummaryScreen extends StatelessWidget {
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.0)),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -33,23 +34,23 @@ class ProjectTeamSummaryScreen extends StatelessWidget {
                         height: 30,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(20),
-                            color: Colors.grey
+                            color: Color(0xffa3f1f7)
                         ),
                       ),
-                      5.widthBox,
+                      10.widthBox,
                       Container(
 
                         decoration:BoxDecoration(
                             borderRadius: BorderRadius.circular(20),
-                            color:Colors.blueGrey
+                            color:Color(0xffe5f6fa)
                         ) ,
                         child: TextWidget(text: "View All",color: primaryColor,).pSymmetric(h: 10,v: 4),
                       ),
-                      10.widthBox,
+                      25.widthBox,
                       Container(
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey[200],
+                          //color: Colors.grey[200],
                         ),
                         child: Row(
                           children: [
@@ -59,22 +60,89 @@ class ProjectTeamSummaryScreen extends StatelessWidget {
                           ],
                         ).pSymmetric(h: 10,v: 5),
                       ),
-                      10.widthBox,
+                      15.widthBox,
                       Container(
                         decoration: BoxDecoration(
                             color: primaryColor,
                             borderRadius: BorderRadius.circular(24)
                         ),
-                        child: const Icon(Icons.settings).p12(),
+                        child: const Icon(Icons.settings,color: Colors.white,).p4(),
                       )
                     ],
                   )
 
                 ]
-            )
+            ),
+            20.heightBox,
+            Wrap(
+              spacing: 10.0,
+              runSpacing: 5.0,
+              children: [
+                tile(
+                    context: context,
+                    title: "My Active Task",
+                    number: "1",
+                    icon: icon1,
+                    color: color1),
+                tile(
+                    context: context,
+                    title: "Task Overdue",
+                    number: "1",
+                    icon: icon2,
+                    color: color2),
+                tile(
+                    context: context,
+                    title: "Task Due Today",
+                    number: "1",
+                    icon: icon3,
+                    color: color3),
+                tile(
+                    context: context,
+                    title: "Task Due this week",
+                    number: "1",
+                    icon: icon4,
+                    color: color4),
+
+              ],
+            ),
           ],
         ).pSymmetric(h: 10,v: 10),
       ).pSymmetric(v: 12),
+    );
+  }
+  tile(
+      {required BuildContext context,
+        required String title,
+        required String number,
+        required String icon,
+        required Color? color}) {
+    return Container(
+      width: context.screenWidth * 0.16,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0), color: color),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextWidget(
+            text: title,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          20.heightBox,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextWidget(
+                text: number,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              icon.image(),
+            ],
+          )
+        ],
+      ).p12(),
     );
   }
 }
