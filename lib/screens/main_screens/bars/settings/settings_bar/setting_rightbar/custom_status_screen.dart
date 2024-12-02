74cf789cd7ebe3892e863368d13c6605c31ab9ff
Khushi Agarwal <khushiagarwal270203@gmail.com>
2024-12-02 15:16:49 +0530

    custom_sttaus

--- lib/screens/main_screens/bars/settings/settings_bar/setting_rightbar/custom_status_screen.dart
+++ lib/screens/main_screens/bars/settings/settings_bar/setting_rightbar/custom_status_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:jio_works/custom_widgets/text_widget.dart';
+import 'package:jio_works/utilities/library.dart';
 
 class CustomStatusScreen extends StatelessWidget {
   const CustomStatusScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Expanded(child: Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextWidget(text: "Manage Status",fontSize: 20,fontWeight: FontWeight.bold,),
+          TextWidget(text: 'Customize statuses and categories to suit your specific workflow setup. Status categories can have multiple statuses to reflect its different stages.',color: Colors.black87,)
+        ],
+      ).p12(),
+    ).pSymmetric(v: 10),);
   }
 }
